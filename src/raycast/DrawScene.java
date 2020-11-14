45710a9e9940783972af7634ff59d9ebebd0fefd
Para-lyzed <73729354+Para-lyzed@users.noreply.github.com>
2020-11-13 20:53:55 -0800

    Improved movement and added rotation (#6)
    * Update README.md
    
    * Day one
    
    * Improvements on movement and graphics

--- src/raycast/DrawScene.java
+++ src/raycast/DrawScene.java
@@ -1,3 +1,14 @@
+/*  
+ *  Title of project
+ * 
+ *  Date of completion
+ * 
+ *  This program was created under the collaboration of Nathan Grimsey, Eric Lumpkin, Dylan Gibbons-Churchward, and Matthew McGuinn
+ *  for Martin Hock's CS143 class in the Fall quarter of 2020.
+ * 
+ *  This code may be found at https://github.com/CS143-Raycasting-Project/Raycast along with documentation.
+ */
+
 package raycast;
 
 import java.awt.*;
@@ -5,14 +16,38 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class DrawScene extends JPanel {
-    private Point playerLocation = new Point();
+    //Points were causing rounding issues, so I just made the coords 2 separate doubles.
+    private double playerX;
+    private double playerY;
+    private int playerRotation = 0; //This is in degrees so that I can just use an int.
 
-    public DrawScene(int x, int y) {
-        this.playerLocation.setLocation(x, y);
+    public DrawScene(double x, double y) {
+        this.playerX = x;
+        this.playerY = y;
     }
 
-    public void setLocation(int x, int y) {
-        playerLocation.setLocation(x, y);
+    public void move(String direction) { //I use some simple trig here to change how the movement is done depending on rotation.
+        if (direction.equals("left")) {
+            playerX -= Math.cos(Math.toRadians(playerRotation));
+            playerY -= Math.sin(Math.toRadians(playerRotation));
+        }
+        else if (direction.equals("right")) {
+            playerX += Math.cos(Math.toRadians(playerRotation));
+            playerY += Math.sin(Math.toRadians(playerRotation));
+        }
+        else if (direction.equals("forwards")) {
+            playerX += Math.sin(Math.toRadians(playerRotation));
+            playerY -= Math.cos(Math.toRadians(playerRotation));
+        }
+        else if (direction.equals("backwards")) {
+            playerX -= Math.sin(Math.toRadians(playerRotation));
+            playerY += Math.cos(Math.toRadians(playerRotation));
+        }
+        
+    }
+
+    public void rotate(int angle) {
+        playerRotation += angle;
     }
 
     public void renderFrame() {
@@ -22,10 +57,12 @@ public class DrawScene extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.BLACK);
-        // Graphics2D g2 = (Graphics2D) g;
-        // g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setColor(Color.WHITE);
-        g.fillRect((int)playerLocation.getX(), (int)playerLocation.getY(), 10, 10);
+        Graphics2D g2d = (Graphics2D) g;
+        // g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); /* We can turn this on later if necessary */
+        g2d.setColor(Color.WHITE);
+        g2d.rotate(Math.toRadians(playerRotation), (int)playerX + 10, (int)playerY + 10);
+        g2d.fillRect((int)playerX, (int)playerY, 20, 20);
+        g2d.drawLine((int)playerX + 10, (int)playerY + 10, (int)playerX + 10, (int)playerY - 10);
     }
 
 }
--- src/raycast/Main.java
+++ src/raycast/Main.java
@@ -1,3 +1,14 @@
+/*  
+ *  Title of project
+ * 
+ *  Date of completion
+ * 
+ *  This program was created under the collaboration of Nathan Grimsey, Eric Lumpkin, Dylan Gibbons-Churchward, and Matthew McGuinn
+ *  for Martin Hock's CS143 class in the Fall quarter of 2020.
+ * 
+ *  This code may be found at https://github.com/CS143-Raycasting-Project/Raycast along with documentation.
+ */
+
 package raycast;
 
 import java.awt.event.*;
@@ -6,58 +17,35 @@ import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class Main extends JFrame implements ActionListener {
-    private static boolean left, right, backwards, forwards, turnLeft, turnRight, render = false;
-    private int playerX, playerY = 0;
-    static Timer keyTimer = new Timer(10, new Main());
-    private static DrawScene scene = new DrawScene(0, 0);
+    private static boolean left, right, backwards, forwards, turnLeft, turnRight, render = false; //These will be used for the movement, and render will be used to determine whether or not a freame needs to be rendered
+    static Timer keyTimer = new Timer(10, new Main()); //This is the clock of the game. It runs a tick every 10ms
+    private static DrawScene scene = new DrawScene(200.0, 200.0); //Calls to the graphics function to draw the scene
 
     public static void main(String[] args) {
+        //Pretty standard graphics setup
         JFrame f = new JFrame();
         f.setSize(400,400);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.addKeyListener(new KeyListener() {
+        f.addKeyListener(new KeyListener() { //This KeyListener is what allows movement inputs to be detected.
+            //If a key is held down during the tick, then the corresponding movement boolean will be true.
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    turnLeft = true;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    turnRight = true;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_W) {
-                    forwards = true;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_A) {
-                    left = true;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_S) {
-                    backwards = true;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_D) {
-                    right = true;
-                }
+                if      (e.getKeyCode() == KeyEvent.VK_LEFT)    { turnLeft  = true; }
+                else if (e.getKeyCode() == KeyEvent.VK_RIGHT)   { turnRight = true; }
+                else if (e.getKeyCode() == KeyEvent.VK_W)       { forwards  = true; }
+                else if (e.getKeyCode() == KeyEvent.VK_A)       { left      = true; }
+                else if (e.getKeyCode() == KeyEvent.VK_S)       { backwards = true; }
+                else if (e.getKeyCode() == KeyEvent.VK_D)       { right     = true; }
             }
-            public void keyTyped(KeyEvent e) {
+            public void keyTyped(KeyEvent e) { //Currently this isn't used, but this will be helpful for actions
 
             }
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    turnLeft = false;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    turnRight = false;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_W) {
-                    forwards = false;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_A) {
-                    left = false;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_S) {
-                    backwards = false;
-                }
-                else if (e.getKeyCode() == KeyEvent.VK_D) {
-                    right = false;
-                }
+            public void keyReleased(KeyEvent e) { //If a key is released during the tick, then the corresponding movement boolean will be false.
+                if      (e.getKeyCode() == KeyEvent.VK_LEFT)     { turnLeft  = false; }
+                else if (e.getKeyCode() == KeyEvent.VK_RIGHT)    { turnRight = false; }
+                else if (e.getKeyCode() == KeyEvent.VK_W)        { forwards  = false; }
+                else if (e.getKeyCode() == KeyEvent.VK_A)        { left      = false; }
+                else if (e.getKeyCode() == KeyEvent.VK_S)        { backwards = false; }
+                else if (e.getKeyCode() == KeyEvent.VK_D)        { right     = false; }
             }
         });
         f.add(scene);
@@ -67,31 +55,42 @@ public class Main extends JFrame implements ActionListener {
 
     }
     public void actionPerformed(ActionEvent arg0) {
+        //There are safeguards to prevent movement when opposing directions are being held, so that frames aren't unnecessarily rendered
         if (!(left && right)) {
             if (left) {
-                playerX --;
+                scene.move("left");
                 render = true;
             }
             else if (right) {
-                playerX ++;
+                scene.move("right");
                 render = true;
             }
         }
         if (!(forwards && backwards)) {
             if (forwards) {
-                playerY --;
+                scene.move("forwards");
                 render = true;
             }
             else if (backwards) {
-                playerY ++;
+                scene.move("backwards");
                 render = true;
             }
         }
+        if (!(turnLeft && turnRight)) {
+            if (turnLeft) {
+                scene.rotate(-1);
+                render = true;
+            }
+            else if (turnRight) {
+                scene.rotate(1);
+                render = true;
+            }
+        }
+        //This makes sure that a frame is only rendered if the player has moved. It's just to reduce CPU load.
+        //Since there is nothing moving other than the player, it makes no sense to render the same screen repeatedly.
         if (render) {
-            scene.setLocation(playerX, playerY);
             scene.renderFrame();
         }
-        // System.out.println(playerX + ", " + playerY);
         render = false;
     }
 }
