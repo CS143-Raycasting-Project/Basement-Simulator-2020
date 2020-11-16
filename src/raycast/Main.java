45602fc60dd73f5b4315b7e1c1c047b06d813b2b
Para-lyzed <73729354+Para-lyzed@users.noreply.github.com>
2020-11-15 20:51:32 -0800

    Fixed resolution variables (#12)
    * Fixed resolution variables
    
    * Fixed resolution stuff

--- src/raycast/Main.java
+++ src/raycast/Main.java
@@ -21,15 +21,13 @@ public class Main extends JFrame implements ActionListener {
     public static int resolution = 400;
     public static int cellSize = resolution / mazeSize;
     private static boolean left, right, backwards, forwards, turnLeft, turnRight, render = false; //These will be used for the movement, and render will be used to determine whether or not a freame needs to be rendered
-    private static Scene scene = new Scene(200.0, 200.0); //Calls to the graphics function to draw the scene
+    private static Scene scene = new Scene(resolution / 2, resolution / 2); //Calls to the graphics function to draw the scene
     static Timer keyTimer = new Timer(10, new Main()); //This is the clock of the game. It runs a tick every 10ms
-    public static int windowX = 400;
-    public static int windowY = 400;
     public static void main(String[] args) {
         //Pretty standard graphics setup
         JFrame f = new JFrame();
 
-        f.setSize(windowX, windowY + 40); /* The 40 is for the window bar */
+        f.setSize(resolution, resolution + 40); /* The 40 is for the window bar */
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.addKeyListener(new KeyListener() { //This KeyListener is what allows movement inputs to be detected.
             //If a key is held down during the tick, then the corresponding movement boolean will be true.
