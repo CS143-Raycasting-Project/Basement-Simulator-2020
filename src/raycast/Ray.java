264da51df749b8aa82f3cfba3527a9ec39efd73f
Kylerace <kylerlumpkin1@gmail.com>
2020-11-25 01:51:15 -0800

    here ya go (#17)

--- src/raycast/Ray.java
+++ src/raycast/Ray.java
@@ -19,10 +19,12 @@ import java.util.Set;
 public class Ray {
     private double x, y;
     private double distanceRatioX, distanceRatioY, sideDistX, sideDistY, rayX, rayY, perpendicularVectX, perpendicularVectY, dirX, dirY;
-    private int nextStepX, nextStepY, playerTurfXIndex, playerTurfYIndex;
+    private int nextStepX, nextStepY, currentTurfXIndex, playerTurfYIndex;
     private boolean hit = false;
     private boolean isYSideOfWall;//this is true if the ray is moving up or down this tick, and false otherwise
     private double adjustedWallDist;
+    private Point pointOfImpact;
+    private double collisionCoord;
     //so many vars reeeeeeeee
     int squaresToCheck = 10000;//this is how many map squares each ray will go through until they give up (if they dont hit anything)
     public Ray (double x, double y, double angle, double xColumn) {
@@ -30,7 +32,7 @@ public class Ray {
         //x and y are the coor
         this.x = x;
         this.y = y;
-        this.playerTurfXIndex = (int)x;//which map square we're in, X AND Y HAVE TO BE DIVIDED BY CELLSIZE WHEN YOU CALL THIS, DO NOT ROUND
+        this.currentTurfXIndex = (int)x;//which map square we're in, X AND Y HAVE TO BE DIVIDED BY CELLSIZE WHEN YOU CALL THIS, DO NOT ROUND
         this.playerTurfYIndex = (int)y;
 
         this.dirX = Math.cos(angle);//dir has a length of 1, so dirX is just the x value of a point on the unit circle
@@ -44,6 +46,7 @@ public class Ray {
         
         this.distanceRatioX = Math.abs(1/rayX);
         this.distanceRatioY = Math.abs(1/rayY);
+        
     }
     
     //move along a single turf at a time and check if its a wall. if it is then return the adjusted euclidian distance. you cant use
@@ -55,10 +58,10 @@ public class Ray {
         int iteration = 0;
         if (rayX < 0) {
             nextStepX = -1;
-            sideDistX = (x - playerTurfXIndex) * distanceRatioX;
+            sideDistX = (x - currentTurfXIndex) * distanceRatioX;
         } else {
             nextStepX = 1;
-            sideDistX = (playerTurfXIndex + 1.0 - x) * distanceRatioX;
+            sideDistX = (currentTurfXIndex + 1.0 - x) * distanceRatioX;
         }
         if (rayY < 0) {
             nextStepY = -1;
@@ -71,24 +74,34 @@ public class Ray {
             //if the ray is looking for a turf in the X direction
             if (sideDistX < sideDistY) {
                 sideDistX += distanceRatioX;
-                playerTurfXIndex += nextStepX;
+                currentTurfXIndex += nextStepX;
+                collisionCoord = sideDistX + x;
                 isYSideOfWall = false;
             } else {//if the ray is looking for a turf in the Y direction
                 sideDistY += distanceRatioY;
+                collisionCoord = sideDistY + y;
                 playerTurfYIndex += nextStepY;
                 isYSideOfWall = true;
             }
-            if (Scene.maze.findTurfByIndex(playerTurfXIndex, playerTurfYIndex).turfType > 0) {//0's are floors, anything greater is a wall type
-                //System.out.println("x "+playerTurfXIndex+" y "+playerTurfYIndex+" type "+Main.raymap.findTurfByIndex(playerTurfXIndex, playerTurfYIndex).turfType);
+            if (Scene.maze.findTurfByIndex(currentTurfXIndex, playerTurfYIndex).turfType > 0) {//0's are floors, anything greater is a wall type
+                //System.out.println("x "+currentTurfXIndex+" y "+playerTurfYIndex+" type "+Main.raymap.findTurfByIndex(currentTurfXIndex, playerTurfYIndex).turfType);
+                //System.out.println(collisionCoord);
                 hit = true;
             }
             iteration++;
         }
         if (isYSideOfWall == false) {
-            adjustedWallDist = (playerTurfXIndex - x + (1 - nextStepX) / 2) / rayX;
+            adjustedWallDist = (currentTurfXIndex - x + (1 - nextStepX) / 2) / rayX;
         } else {
             adjustedWallDist = (playerTurfYIndex - y + (1 - nextStepY) / 2) / rayY;
         }
         return adjustedWallDist;
     }
-}
+    /**
+     * returns the value for this ray's collision coordinate
+     * @return
+     */
+    public double getCollisionCoord() {
+        return collisionCoord;
+    }
+} 
