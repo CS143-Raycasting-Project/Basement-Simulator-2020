e5f9799d0019601b04d2a287bb5e8b7d8c8fa253
Para-lyzed <73729354+Para-lyzed@users.noreply.github.com>
2020-12-10 18:33:02 -0800

    Renamed Links (#35)
    * Final commit with bug fixes and cleanup
    
    * Renamed links

--- src/bs2020/Texture.java
+++ src/bs2020/Texture.java
@@ -6,7 +6,7 @@
  *  This program was created under the collaboration of Nathan Grimsey, Eric Lumpkin, Dylan Gibbons-Churchward, and Matthew McGuinn
  *  for Martin Hock's CS143 class in the Fall quarter of 2020.
  * 
- *  This code may be found at https://github.com/CS143-Raycasting-Project/Raycast along with documentation.
+ *  This code may be found at https://github.com/CS143-Raycasting-Project/Basement-Simulator-2020 along with documentation.
  */
 
 package bs2020;
